13866bb8a82276bf6dfe61d28425cd1fdff4712b
Willmanu <pamanuwill@gmail.com>
2026-01-09 21:13:36 -0300

    Criando formulario

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :contacts
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'static_page#index'
 
   get 'sobre', to: 'static_page#sobre'
   get 'contato', to: 'static_page#contato'
+
+  resources :contacts
+  resources :users, only: %i[new create]
 end
