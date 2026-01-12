dab373d34802de45eaa6d3d6b3394ae3d491f8cb
Willmanu <pamanuwill@gmail.com>
2026-01-11 23:36:29 -0300

    Autenticando usuário,mostrando se este está logadop ou não, bloqueando rotas etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  # get "sessions/new"
+  # get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'static_page#index'
 
   get 'sobre', to: 'static_page#sobre'
   get 'contato', to: 'static_page#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
 
   resources :contacts
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
 end
